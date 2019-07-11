3de93f988e794163a5b65b0d761f63cc411b395e
Paul Bright <paulbright@gmail.com>
2019-07-11 15:47:53 +0000

    mosquitto userid password

--- gsm_test.py
+++ gsm_test.py
@@ -6,7 +6,7 @@ from random import randrange
 GPIO.setmode(GPIO.BOARD)    
  
 # Enable Serial Communication
-port = serial.Serial("/dev/ttyUSB0", baudrate=115200, timeout=1)
+port = serial.Serial("/dev/ttyS0", baudrate=115200, timeout=1)
  
 # Transmitting AT Commands to the Modem
 # '\r\n' indicates the Enter key
